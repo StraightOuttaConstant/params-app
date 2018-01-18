799df878bcb64c61963a0ad8801503be16ee09ee
JackMarx <seriousfools@gmail.com>
2018-01-17 21:04:22 -0600

    url segments

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -4,4 +4,20 @@ class ParamsExamplesController < ApplicationController
     input_message_2 = params[:my_message_2]
     render json: {message: input_message, message_2: input_message_2}
   end
+
+  def segment_params_method
+    input_message = params[:this_is_a_variable]
+    second_message = params[:coffee]
+    render json: {value: input_message, second_value: second_message}
+  end
 end
+
+
+
+
+
+
+
+
+
+
